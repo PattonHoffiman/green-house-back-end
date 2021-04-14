0d95a76f9da73988b2fb14a611c046e07781c84b
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-04-14 08:44:31 -0300

    [Fix] Session now delete all old notifications

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -4,15 +4,18 @@ import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import VerifyWaterDayDateService from '@modules/plants/services/VerifyWaterDayDateService';
+import DeleteNotificationService from '@modules/notifications/services/DeleteNotificationService';
 
 export default class SessionsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
     const authenticateUser = container.resolve(AuthenticateUserService);
     const verifyWaterDayDate = container.resolve(VerifyWaterDayDateService);
+    const deleteNotifications = container.resolve(DeleteNotificationService);
 
     const { user, token } = await authenticateUser.execute({ email, password });
     await verifyWaterDayDate.execute(user.id);
+    await deleteNotifications.execute(user.id);
 
     return res.status(200).send({
       token,
